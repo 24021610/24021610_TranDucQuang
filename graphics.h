87cc8d1b49bd9c2261c0ae9212e452fd9f2a50e7
24021610 <24021610@vnu.edu.vn>
2025-02-14 15:31:26 +0700

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,26 +1,47 @@
-#ifndef GRAPHICS_H_INCLUDED
-#define GRAPHICS_H_INCLUDED
+#ifndef GRAPHICSH
+#define GRAPHICSH
 
 #include <SDL.h>
 #include <iostream>
 #include <SDL_image.h>
 #include "header.h"
-
-using namespace std;
+#include "logic.h"
 
     struct Graphics{
         SDL_Window* window;
         SDL_Renderer* renderer;
+        SDL_Texture* cell_empty, *cell_o, *cell_x, *o_win, *x_win;
 
     void clearRenderer(){
     SDL_RenderClear(renderer);
     }
 
+    void prepareScene(SDL_Texture* texture){
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+        }
+
     void logErrorAndExit(const char* msg, const char* error){
             SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s : %s", msg, error);
             SDL_Quit();
         }
-    void init(){
+
+        SDL_Texture* loadTexture(const char* filename){
+            SDL_Texture* texture = IMG_LoadTexture(renderer, filename);
+            if (texture == nullptr) SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load Texture: %s", IMG_GetError());
+            return texture;
+        }
+    void renderxy(Tictactoe game){
+        SDL_Rect rect;
+            rect.x = 100;
+            rect.y = 100;
+            rect.w = 600;
+            rect.h = 400;
+        (game.currentMove == X_CELL)? SDL_RenderCopy(renderer, x_win, NULL, &rect) : SDL_RenderCopy(renderer, o_win, NULL, &rect);
+        presentScene();
+    }
+
+    void initSDL(){
         window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (window == nullptr) logErrorAndExit("Create Window: ", SDL_GetError());
 
@@ -31,55 +52,58 @@ using namespace std;
 
 
         if (!IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG)) logErrorAndExit("Load Image: ", IMG_GetError());
+        cell_empty = loadTexture("cell_empty.PNG");
+        cell_o = loadTexture("cell_o.PNG");
+        cell_x = loadTexture("cell_x.PNG");
+        o_win = loadTexture("o_win.PNG");
+        x_win = loadTexture("x_win.PNG");
+
     }
-
-        void prepareScene(SDL_Texture* texture){
-            SDL_RenderClear(renderer);
-            SDL_RenderCopy(renderer, texture, NULL, NULL);
-
-        }
-
         void presentScene(){
         SDL_RenderPresent(renderer);
         }
-
-        SDL_Texture* loadTexture(const char* filename){
-            SDL_Texture* texture = IMG_LoadTexture(renderer, filename);
-            if (texture == nullptr) SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load Texture: %s", IMG_GetError());
-            return texture;
-        }
         void RenderTexture (SDL_Texture* texture, int x, int y){
-            SDL_RenderClear(renderer);
             SDL_Rect rect;
             rect.x = x;
             rect.y = y;
             SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
             SDL_RenderCopy(renderer,texture,NULL, &rect);
+            presentScene();
         }
+
+        void render(const Tictactoe& game) {
+
+
+        for (int i = 0; i < BOARD_SIZE; i++){
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                int x = BOARD_X + j * CELL_SIZE;
+                int y = BOARD_Y + i * CELL_SIZE;
+                switch (game.board[i][j]) {
+                    case EMPTY_CELL: RenderTexture(cell_empty, x, y); break;
+                    case X_CELL: RenderTexture(cell_x, x, y); break;
+                    case O_CELL: RenderTexture(cell_o, x, y); break;
+                };
+            }
+        }
+
+        presentScene();
+    }
+
+
         void sdlQuit(){
             SDL_DestroyRenderer(renderer);
+            SDL_DestroyTexture(cell_empty);
+            SDL_DestroyTexture(cell_o);
+            SDL_DestroyTexture(cell_x);
             SDL_DestroyWindow(window);
             IMG_Quit();
             SDL_Quit();
-        }
-        void waitUntilKeyPressed(){
-    SDL_Event e;
-     while(true){
-        if (( SDL_PollEvent(&e) !=0 ) && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT)){
-            return;
-            }
-            SDL_Delay(700);
+
+
     }
-}
-    void drawSomething() {
-    SDL_Rect filled_rect;
-    filled_rect.x = SCREEN_WIDTH - 400;
-    filled_rect.y = SCREEN_HEIGHT - 150;
-    filled_rect.w = 320;
-    filled_rect.h = 100;
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // green
-    SDL_RenderFillRect(renderer, &filled_rect);
-}
-    };
+};
+
+
+
 #endif // GRAPHICS_H_INCLUDED
 

a10831fa5abb6e0b4ec3533c51f2af0a8fcc8efc
24021610 <24021610@vnu.edu.vn>
2025-02-17 17:39:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 #include "header.h"
 #include "logic.h"
 
-
-
 int main(int argc, char *argv[])
 {
      Graphics graphics;
      ScrollingBackground background;
-     object spike;
      Mouse mouse;
 
      graphics.initSDL();
@@ -18,12 +15,8 @@ int main(int argc, char *argv[])
      mouse.x=100;
      mouse.y=100;
 
-     spike.x = 400;
-     spike.y=400;
-
      mouse.texture = graphics.loadTexture ("Model.PNG");
      background.setTexture(graphics.loadTexture("ahaha.PNG"));
-     spike.texture = graphics.loadTexture ("spike.PNG");
 
     SDL_Event event;
     while(true){
@@ -31,27 +24,18 @@ int main(int argc, char *argv[])
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT) graphics.sdlQuit();
 
-            const Uint8* currentKeyState = SDL_GetKeyboardState(NULL);
-            if (currentKeyState[SDL_SCANCODE_LEFT]) mouse.turnWest();
-            if(currentKeyState[SDL_SCANCODE_RIGHT]) mouse.turnEast();
-            if(currentKeyState[SDL_SCANCODE_SPACE]) mouse.jump();
+            if(event.type == SDL_KEYDOWN){
+                if(event.key.keysym.sym == SDLK_w) mouse.jump();
+                    }
 
             SDL_RenderClear(graphics.renderer);
             graphics.renderBackground(background);
-            background.scroll(1);
             renderModel(mouse, graphics);
-            renderSpike(spike, graphics);
             mouse.move();
             graphics.presentScene();
             SDL_Delay(1);
-        }
+                                    }
 
-    }
+            }
     return 0;
 }
-
-
-
-
-
-
